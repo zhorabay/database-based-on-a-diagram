560b0769dab3d695ccc0cd1401c0cd8391b927b6
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-30 16:50:52 +0200

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE clinic
+
 CREATE TABLE patients (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     name varchar,
     date_of_birth date,
     CONSTRAINT patients_pkey PRIMARY KEY (id)
 );
+
 CREATE TABLE medical_histories (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     admitted_at timestamp,
@@ -12,12 +14,30 @@ CREATE TABLE medical_histories (
     status varchar,
     CONSTRAINT medical_histories_pkey PRIMARY KEY (id)
 );
+
+-- Creating a foreign key relationship between the "medical_histories" table and the "patients" table.
+ALTER TABLE medical_histories
+ADD CONSTRAINT fk_patient_id
+FOREIGN KEY (patient_id)
+REFERENCES patients(id);
+
 CREATE TABLE treatments (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     type varchar,
     name varchar,
     CONSTRAINT treatments_pkey PRIMARY KEY (id)
 );
+
+-- Create a new table which connects the "medical_histories" table and the "treatments" table.
+CREATE TABLE medical_history_treatments (
+    medical_history_id int,
+    treatment_id int,
+    FOREIGN KEY (medical_history_id)
+    REFERENCES medical_histories(id),
+    FOREIGN KEY (treatment_id)
+    REFERENCES treatment(id)
+);
+
 CREATE TABLE invoices (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     total_amount decimal,
@@ -26,6 +46,13 @@ CREATE TABLE invoices (
     medical_history_id int,
     CONSTRAINT invoices_pkey PRIMARY KEY (id)
 );
+
+-- Create a foreign key for invoices.
+ALTER TABLE invoices
+ADD CONSTRAINT fk_invoice_id
+FOREIGN KEY (medical_history_id)
+REFERENCES medical_histories (id);
+
 CREATE TABLE invoice_items (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     unit_price decimal,
@@ -35,3 +62,12 @@ CREATE TABLE invoice_items (
     treatement_id int,
     CONSTRAINT invoice_items_pkey PRIMARY KEY (id)
 );
+
+--Create foreign keys for invoice_items.
+ALTER TABLE invoice_items
+ADD CONSTRAINT fk_invoice_items_invoice_id
+FOREIGN KEY (invoice_id)
+REFERENCES invoices (id),
+ADD CONSTRAINT fk_invoice_items_treatment_id
+FOREIGN KEY (treatment_id)
+REFERENCES treatments (id);
